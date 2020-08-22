00684832b901c2b5a97fac0488b95fb61b657141
Sandeep Chugh <sandeep.chugh@live.com>
2020-08-21 23:42:49 -0500

    added docker file to all microservices

--- query/index.js
+++ query/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
 const app = express();
 
 app.use(bodyParser.json());
