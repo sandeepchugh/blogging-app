8e0ce8a3155cdf6daf1fc8f221e23118b6adc8ff
Sandeep Chugh <sandeep.chugh@live.com>
2020-08-21 22:51:02 -0500

    added support for service restarts and event sourcing with the event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,22 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
+app.get('/events', (req,res) => {
+    res.send(events);
+});
+
 app.post('/events',(req,res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
     axios.post('http://localhost:4003/events', event);
-    
+
     res.send({ status: 'OK' });
 });
 
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
@@ -9,12 +10,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req,res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req,res) => {
-    const {type, data} = req.body;
+const handleEvent = (type,data) => {
     if (type === "PostCreated"){
         const {id, title} = data;
         
@@ -39,10 +35,28 @@ app.post('/events', (req,res) => {
         comment.content = content;
         comment.status = status;
     }
+};
+
+app.get('/posts', (req,res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req,res) => {
+    const {type, data} = req.body;
+    
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002');
+
+    res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data){
+        console.log('Provessing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
